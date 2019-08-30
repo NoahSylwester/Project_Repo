f9d8383fedd236057afba15ef4b5d553c5595ce8
Noah Sylwester <nanoabram@comcast.net>
2019-08-29 21:13:00 -0700

    add javascript, feed functionality and initial pet creation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,41 @@
 // player currency
-var chippies = 0;
+var chippies = 200;
 
 
 
 // this constructor creates a new netpet
-var NetPet = function(hunger) {
-  this.hunger = hunger;
+var NetPet = function(hungerRate) {
+  this.hungerValue = 100;
+  this.hungerRate = hungerRate;
 
-  this.getFed = function(foodValue) {
-    this.hunger -= foodValue;
+  this.getHungrier = function() {
+    hungerValue += hungerRate;
+  };
+
+  this.feed = function(foodValue) {
+    this.hungerValue += foodValue;
+    if (this.hungerValue >= 200) { // limit amount of feeding
+      this.hungerValue = 200;
+      $('.pet-display').text("I'm full!"); // .pet-display working class
+    };
+    $('.status-bar').text('Hunger: ' + this.hungerValue); // .status-bar working class
   };
 }
+
+// this constructor creates food items
+var FoodItem = function(foodValue) {
+  this.foodValue = foodValue;
+}
+
+// initialize new pet for testing
+var Brian = new NetPet(3);
+
+// create food
+var cookie = new FoodItem(10);
+
+
+// feed pet with feed button
+$('#feed-button').on('click', function(event) {
+  event.preventDefault();
+  Brian.feed(cookie.foodValue);
+});
